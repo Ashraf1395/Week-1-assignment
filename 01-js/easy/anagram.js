a7f0a6fdd097215cea4c93a1c1ea3f0bfc49c79e
Ashraf1395 <122676159+Ashraf1395@users.noreply.github.com>
2023-06-14 11:42:18 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,6 +8,23 @@
 */
 
 function isAnagram(str1, str2) {
+  let a = str1;
+	let b = str2;
+  // Splitting the string according to characters and storing them in an array
+	var array_a = a.split('');
+	var array_b = b.split('');
+  // Sorting the arrays
+	var sorted_a= array_a.sort();
+	var sorted_b= array_b.sort();
+  // if sorted arrays have same lengths and both are exaclty equal they are anagrams else they are not
+	if (sorted_a.length == sorted_b.length){
+		return JSON.stringify(sorted_a)==JSON.stringify(sorted_b);
+  }
+  else{
+    return false;
+  }
+}
+
 
 }
 
